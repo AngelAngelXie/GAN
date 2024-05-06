aabe0bc6d978e55d317d3633005d889c136e1a31
Angel Xie <angel.xie4956@gmail.com>
2024-05-07 00:58:23 +0800

    tunning

--- skeleton_code/models.py
+++ skeleton_code/models.py
@@ -156,11 +156,11 @@ class DCDiscriminator(nn.Module):
     def forward(self, x):
 
         out = F.leaky_relu(self.conv1(x), negative_slope=0.2)
-        out = self.dropout1(out)
+        # out = self.dropout1(out)
         out = F.leaky_relu(self.conv2(out), negative_slope=0.2)
-        out = self.dropout2(out)
+        # out = self.dropout2(out)
         out = F.leaky_relu(self.conv3(out), negative_slope=0.2)
-        out = self.dropout3(out)
+        # out = self.dropout3(out)
 
         out = self.conv4(out).squeeze()
         out = F.sigmoid(out)
