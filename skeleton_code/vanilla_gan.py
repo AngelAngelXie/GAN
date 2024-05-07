12bece25a4e17d10291c361cb57c0fc5f4416659
Angel Xie <angel.xie4956@gmail.com>
2024-05-07 11:36:49 +0800

    tunning

--- skeleton_code/vanilla_gan.py
+++ skeleton_code/vanilla_gan.py
@@ -40,8 +40,8 @@ def train(train_loader, opts, device):
     D.to(device)
     
     g_optimizer = optim.Adam(G.parameters(), opts.lr, [opts.beta1, opts.beta2])
-    # d_optimizer = optim.Adam(D.parameters(), opts.lr, [opts.beta1, opts.beta2])
-    d_optimizer = optim.SGD(D.parameters(), opts.lr, momentum=0.5)
+    d_optimizer = optim.Adam(D.parameters(), opts.lr, [opts.beta1, opts.beta2])
+    # d_optimizer = optim.SGD(D.parameters(), opts.lr, momentum=0.5)
 
     fixed_noise = sample_noise(opts.batch_size, opts.noise_size).to(device)
     
