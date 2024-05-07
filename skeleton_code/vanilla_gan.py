6d9db9736726b324ee725fc2ad5b3eccb65ea2d5
Angel Xie <angel.xie4956@gmail.com>
2024-05-07 11:33:34 +0800

    tunning

--- skeleton_code/vanilla_gan.py
+++ skeleton_code/vanilla_gan.py
@@ -189,7 +189,7 @@ def create_parser():
     parser.add_argument('--noise_size', type=int, default=100)
 
     # Training hyper-parameters
-    parser.add_argument('--num_epochs', type=int, default=100) # default 50
+    parser.add_argument('--num_epochs', type=int, default=50) # default 50
     parser.add_argument('--batch_size', type=int, default=32, help='The number of images in a batch.') # default was 16
     parser.add_argument('--num_workers', type=int, default=0, help='The number of threads to use for the DataLoader.')
     parser.add_argument('--lr', type=float, default=0.0002, help='The learning rate (default 0.0003)')
