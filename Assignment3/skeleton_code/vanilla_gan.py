4af24662c44d2100525dc08793ca10f16956b232
Angel Xie <angel.xie4956@gmail.com>
2024-05-08 13:46:05 +0800

    debug

--- Assignment3/skeleton_code/vanilla_gan.py
+++ Assignment3/skeleton_code/vanilla_gan.py
@@ -158,13 +158,13 @@ def train(train_loader, opt, device):
         plt.figure(figsize=(8, 8))
         plt.axis('off')
         plt.title(f'Generated images at epoch {i}')
-        plt.imshow(np.transpose(log_dict['images_from_noise_per_epoch'][i], (1, 2, 0)))
+        plt.imshow(np.transpose(genRes[i], (1, 2, 0)))
         plt.show()
         
         plt.figure(figsize=(8, 8))
         plt.axis('off')
         plt.title(f'Generated images after last epoch')
-        plt.imshow(np.transpose(log_dict['images_from_noise_per_epoch'][-1], (1, 2, 0)))
+        plt.imshow(np.transpose(genRes[-1], (1, 2, 0)))
         plt.show()
     
     
